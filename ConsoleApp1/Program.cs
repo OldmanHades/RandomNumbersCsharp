1a587f9af615abe91549d3fe889c289d088ed9b5
Timothy Pecoraro <devilsalias@gmail.com>
2022-07-17 16:00:08 -0500

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace RandomNumberGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int num1 = random.Next(1,100);
+            int num2 = random.Next(1, 100);
+            int num3 = random.Next(1, 100);
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
